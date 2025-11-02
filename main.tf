e5ccb29768022bc5d96edba86cc63daeec8e4f8d
Praveen Ambati <praveenambati1233@gmail.com>
2025-11-02 10:10:56 +0530

    Update Terraform backend organization and workspace

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "remote" {
+    organization = "ambati-org"
+
+    workspaces {
+      name = "dev"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-example"
+  location = "eastus"
+}
